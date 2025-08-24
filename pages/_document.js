283c93fc05ea79b2e20840e9d6049d5262bd6ec3
Kenny Whyte <kawhyte@gmail.com>
2025-08-24 15:15:22 -0700

    feat: update favicon
    - Added favicon links to the document head to ensure the new icons are displayed correctly in the browser tab.

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,9 @@ export default function Document() {
       <Head>
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png"></link>
+        <link rel="icon" type="image/png" sizes="192x192" href="/icons/icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="72x72" href="/icons/icon-72x72.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/icons/icon-96x96.png" />
         <meta name="theme-color" content="#f8f3ef" /> {/* Playful theme background */}
       </Head>
       <body>
