6cdea4fa922fed6ba9f9d9e72563fca39064ef95
Kenny Whyte <kawhyte@gmail.com>
2025-08-26 14:51:19 -0700

    updated scroll

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,26 @@
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import '../styles/globals.css';
 import '../styles/background.css';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.pathname === '/') {
+      document.body.classList.add('overflow-hidden');
+    } else {
+      document.body.classList.remove('overflow-hidden');
+    }
+
+    // Cleanup function to remove the class when the component unmounts or the route changes
+    return () => {
+      document.body.classList.remove('overflow-hidden');
+    };
+  }, [router.pathname]);
+
   return <Component {...pageProps} />;
 };
 
 export default MyApp;
-
