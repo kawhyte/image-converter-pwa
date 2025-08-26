c8e5c3f72087ff39306603d501421a0cfc7c0fc1
Kenny Whyte <kawhyte@gmail.com>
2025-08-26 11:22:25 -0700

    updated favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,11 +5,9 @@ const Document: React.FC = () => {
     <Html lang="en">
       <Head>
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png"></link>
-        <link rel="icon" type="image/png" sizes="192x192" href="/icons/icon-192x192.png" />
-        <link rel="icon" type="image/png" sizes="72x72" href="/icons/icon-72x72.png" />
-        <link rel="icon" type="image/png" sizes="96x96" href="/icons/icon-96x96.png" />
-        <meta name="theme-color" content="#f8f3ef" /> {/* Playful theme background */}
+        <link rel="apple-touch-icon" href="/icons/logo.png" />
+        <link rel="icon" href="/icons/logo.png" />
+        <meta name="theme-color" content="#f8f3ef" /> 
       </Head>
       <body>
         <Main />
